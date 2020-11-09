b0916182414379400b2ae0ac97f4d153791beed2
AmirHossein <amiryousefi98@gmail.com>
2020-11-09 15:18:55 +0330

    phase1_v0

---
+++ Phase1 /test.py
@@ -0,0 +1,18 @@
+from string import punctuation
+import csv
+import pandas as pd
+
+df = pd.read_csv('ted_talks.csv', usecols=['title', 'description'])
+# print(df)
+df.to_csv('new.csv', index=False)
+my_dict = {}
+with open('new.csv', 'r') as fh:
+    rd = csv.DictReader(fh, delimiter=',')
+    for row in rd:
+        new_key = row['title']
+        new_values = (row['description'])
+        my_dict[new_key] = new_values
+
+print(my_dict)
+# print("punctuation : ",punctuation)
+punck=list(punctuation)
