efa9a81df7c46482186c8dfea1d01d3a6ebbc760
AmirHossein <amiryousefi98@gmail.com>
2020-11-09 17:13:54 +0330

    .

--- Phase1 /test.py
+++ Phase1 /test.py
@@ -1,6 +1,7 @@
 from string import punctuation
 import csv
 import pandas as pd
+from lxml import etree
 
 df = pd.read_csv('ted_talks.csv', usecols=['title', 'description'])
 # print(df)
@@ -14,5 +15,10 @@ with open('new.csv', 'r') as fh:
         my_dict[new_key] = new_values
 
 print(my_dict)
+tree = etree.parse('Persian.xml')
+root = tree.getroot()
+print(root[0][3].tag)
+
+
 # print("punctuation : ",punctuation)
-punck=list(punctuation)
+punck = list(punctuation)
