56c2e13310af707de88d650aa39aaca5fd30da5a
MohammadAzari <mazard@protonmail.com>
2020-11-10 00:11:11 +0330

    some changes

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ with open('new.csv', 'r') as fh:
 #print(my_dict)
 # print("punctuation : ",punctuation)
 punck=list(punctuation)
-punct = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+punct = r"!\"#$،%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 
 title_token_list = []
