bf323ca7199fbb7176b33b7e3a694b04d2bdc9bd
MohammadAzari <mazard@protonmail.com>
2020-11-10 14:26:22 +0330

    Update eng_preprocess.py

--- eng_preprocess.py
+++ eng_preprocess.py
@@ -16,22 +16,17 @@ with open('new.csv', 'r') as fh:
         new_values = (row['description'])
         my_dict[new_key] = new_values
 
-#print(my_dict)
-# print("punctuation : ",punctuation)
-punck=list(punctuation)
-
-title_token_list = []
-desc_token_list = []
+token_list = []
 for key, value in my_dict.items():
-     title_token_list += nltk.word_tokenize(key)
-     desc_token_list += nltk.word_tokenize(value)
-for item in title_token_list:
+     token_list += nltk.word_tokenize(key)
+     token_list += nltk.word_tokenize(value)
+
+#print(my_dict)
+punck=list(punctuation)
+punck += ["``","''","`","'","--","’"]
+# print(punck)
+
+for item in token_list:
     if item in punck:
-        title_token_list.remove(item)
-for item in desc_token_list:
-    if item in punck:
-        desc_token_list.remove(item)
-tagged_title = nltk.pos_tag(title_token_list)
-tagged_desc = nltk.pos_tag(desc_token_list)
-#print(tagged_title)
-#print(tagged_desc)
+        token_list.remove(item)
+print(token_list)
