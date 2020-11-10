921523cd9c55531d6bc44f865bdde788a5d795fb
AmirHossein <amiryousefi98@gmail.com>
2020-11-10 14:01:01 +0330

    persian_preprocess_v2

--- persian_preprocess.py
+++ persian_preprocess.py
@@ -8,15 +8,14 @@ from hazm import *
 puncf = r"!\"#$،%&'()*+,-.؟/:;<=>?@[\]^_`{|}~"
 punct = list(puncf)
 
-persian_dict = {}
 tree = etree.parse('Persian.xml')
 root = tree.getroot()
 persian_title_token = []
 persian_desc_token = []
-tagged_per_title = []
-tagged_per_desc = []
+# tagged_per_title = []
+# tagged_per_desc = []
 persian_doc_counter = 0
-tagger = POSTagger(model='resources/postagger.model')
+# tagger = POSTagger(model='resources/postagger.model')
 for child in root:
     title = word_tokenize(child.findall(".//{http://www.mediawiki.org/xml/export-0.10/}title")[0].text)
     desc = word_tokenize(child.findall(".//{http://www.mediawiki.org/xml/export-0.10/}text")[0].text)
@@ -25,9 +24,8 @@ for child in root:
 for item in persian_title_token:
     if item in punct:
         persian_title_token.remove(item)
-
+print(persian_title_token)
 for item in persian_desc_token:
     if item in punct:
         persian_desc_token.remove(item)
-print(persian_title_token)
 print(persian_desc_token)
