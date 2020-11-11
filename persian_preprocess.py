efa78328ca4bdd9fd9286f927811df895c8dff24
AmirHossein <amiryousefi98@gmail.com>
2020-11-11 15:28:48 +0330

    persian_preprocess

--- persian_preprocess.py
+++ persian_preprocess.py
@@ -4,9 +4,10 @@ import pandas as pd
 from lxml import etree
 import nltk
 from hazm import *
+from hazm import stopwords_list
 
-puncf = r"!\"#$،%&'()*+,-.؟/:;<=>?@[\]^_`{|}~"
-punct = list(puncf)
+puncf = "!\"#$،%&'()*+,-.؟/::;<=>?@[\]^_`{|}~"
+punct = ["~", "{", "|", "}", ":", "؟", "?", "(", ")", "!", ".", "،", "[", "]", "<", ">", "="]
 
 tree = etree.parse('Persian.xml')
 root = tree.getroot()
@@ -15,18 +16,36 @@ persian_desc_token = []
 # tagged_per_title = []
 # tagged_per_desc = []
 persian_doc_counter = 0
-# tagger = POSTagger(model='resources/postagger.model')
+normalizer = Normalizer()
+stemmer = Stemmer()
+stop_words = []
+stop_words += stopwords_list()
+stop_words += punct
+
+
+# print(stop_words)
+
+
+def delete_stop_word(token):
+    temp = token
+    for item in temp:
+        if item in stop_words:
+            print(item)
+            temp.remove(item)
+    return temp
+
+
+print(punct)
 for child in root:
-    title = word_tokenize(child.findall(".//{http://www.mediawiki.org/xml/export-0.10/}title")[0].text)
-    desc = word_tokenize(child.findall(".//{http://www.mediawiki.org/xml/export-0.10/}text")[0].text)
-    persian_title_token += title
-    persian_desc_token += desc
+    persian_doc_counter = persian_doc_counter + 1
+    title_normal = normalizer.normalize(child.findall(".//{http://www.mediawiki.org/xml/export-0.10/}title")[0].text)
+    desc_normal = normalizer.normalize(child.findall(".//{http://www.mediawiki.org/xml/export-0.10/}text")[0].text)
+    title = word_tokenize(title_normal)
+    desc = word_tokenize(desc_normal)
+    persian_title_token += delete_stop_word(title)
+    persian_desc_token += delete_stop_word(desc)
+    if persian_doc_counter == 1:
+        break
+
 print(len(persian_desc_token))
-for item in persian_title_token:
-    if item in punct:
-        persian_title_token.remove(item)
-print(persian_title_token)
-for item in persian_desc_token:
-    if item in punct:
-        persian_desc_token.remove(item)
 print(persian_desc_token)
